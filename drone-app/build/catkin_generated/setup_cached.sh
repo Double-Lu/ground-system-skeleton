464d8da795429f6df50540c8c66c1cf38fcedf1c
Tyler Thome <tyler_thome@yahoo.com>
2016-10-15 14:42:01 -0700

    add basic ROS/catkin workspace

---
+++ drone-app/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/tyler/Desktop/ground-system-skeleton/drone-app/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/tyler/Desktop/ground-system-skeleton/drone-app/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/tyler/Desktop/ground-system-skeleton/drone-app/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/tyler/Desktop/ground-system-skeleton/drone-app/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/tyler/Desktop/ground-system-skeleton/drone-app/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/tyler/Desktop/ground-system-skeleton/drone-app/src:$ROS_PACKAGE_PATH"
---
+++ drone-app/build/drone_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tyler/Desktop/ground-system-skeleton/drone-app/src/drone_test
+# Build directory: /home/tyler/Desktop/ground-system-skeleton/drone-app/build/drone_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
